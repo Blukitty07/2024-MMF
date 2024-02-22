e460df42bb18605344275f4aa0dbbb0d7b6ff311
Blukitty07 <159348363+Blukitty07@users.noreply.github.com>
2024-02-22 14:31:08 +1300

    Add files via upload
    Comment attempt 1:
    The currently assumedly finished ticket cost and profit data sheet that makes a list of multiple variables [name, cost, profit]

--- 08_ticket_cost.py
+++ 08_ticket_cost.py
@@ -1,6 +1,12 @@
 import pandas
 
-#dictionaries to hold ticket details
+
+# currency formatting function
+def currency(x):
+    return "{:.2f}".format(x)
+
+
+# dictionaries to hold ticket details
 all_names = ["a", "b", "c", "d", "e"]
 all_ticket_costs = [7.50, 7.50, 10.50, 10.50, 6.50]
 surcharge = [0, 0, 0.53, 0.53, 0]
@@ -11,23 +17,36 @@ mini_movie_dict = {
     "Surcharge": surcharge
 }
 
-
 mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+mini_movie_frame = mini_movie_frame.set_index('Name')
 
-#calculate the total ticket cost (ticket + surcharge)
-mini_movie_frame['Total'] = mini_movie_frame ['Surcharge'] \
+# calculate the total ticket cost (ticket + surcharge)
+mini_movie_frame['Total'] = mini_movie_frame['Surcharge'] \
                             + mini_movie_frame['Ticket Price']
 
-#calculate the profit for each ticket
+# calculate the profit for each ticket
 mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
 
-#calculate ticket and profit totals
+
+# calculate ticket and profit totals
 total = mini_movie_frame['Total'].sum()
 profit = mini_movie_frame['Profit'].sum()
 
-#output table with ticket data
+# Currency formatting (uses currency function)
+add_dollars = ['Ticket Price', 'Surcharge', 'Total', 'Profit']
+for var_item in add_dollars:
+    mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
+
+print("~ ~ ~ ~ Ticket Data ~ ~ ~ ~")
+print()
+
+
+# output table with ticket data
 print(mini_movie_frame)
 
-#output total ticket sales and profit
+print()
+print("~ ~ ~ ~ Ticket Cost and Profit ~ ~ ~ ~")
+
+# output total ticket sales and profit
 print("Total Ticket Sales: ${:.2f}".format(total))
 print("Total Profit: ${:.2f}".format(profit))
